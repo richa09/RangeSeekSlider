7f20a2166a54efdcb117159a352f653ae8d1ed3c
richa09 <richa09@users.noreply.github.com>
2018-04-26 15:29:23 +0530

    Swift 4 compatibility

--- Sources/RangeSeekSlider.swift
+++ Sources/RangeSeekSlider.swift
@@ -487,13 +487,13 @@ import UIKit
         } else {
             maxLabel.string = numberFormatter.string(from: selectedMaxValue as NSNumber)
         }
-
+        
         if let nsstring = minLabel.string as? NSString {
-            minLabelTextSize = nsstring.size(attributes: [NSFontAttributeName: minLabelFont])
+            minLabelTextSize = nsstring.size(withAttributes: [NSAttributedStringKey.font: minLabelFont])
         }
 
         if let nsstring = maxLabel.string as? NSString {
-            maxLabelTextSize = nsstring.size(attributes: [NSFontAttributeName: maxLabelFont])
+            maxLabelTextSize = nsstring.size(withAttributes: [NSAttributedStringKey.font: maxLabelFont])
         }
     }
 
